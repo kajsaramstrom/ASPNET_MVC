86e6964b09d7f9c2457463484e3897b023b61815
Kajsa <kajsaramstrom@live.se>
2024-02-21 15:26:19 +0100

    account-details completed

---
+++ WebApp/Models/AccountDetailsAddressModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models;
+
+public class AccountDetailsAddressModel
+{
+    [Display(Name = "Address line 1", Prompt = "Enter your address line", Order = 0)]
+    [Required(ErrorMessage = "Address line is required")]
+    public string AddressLineOne { get; set; } = null!;
+
+    [Display(Name = "Address line 2", Prompt = "Enter your second address line", Order = 1)]
+    public string? AddressLineTwo { get; set; }
+
+    [Display(Name = "Postal code", Prompt = "Enter your postal code", Order = 2)]
+    [DataType(DataType.PostalCode)]
+    [Required(ErrorMessage = "Postal code is required")]
+    [RegularExpression(@"^\d{3} \d{2}$", ErrorMessage = "Please enter a valid postal code in the format 000 00.")]
+    public string PostalCode { get; set; } = null!;
+
+    [Display(Name = "City", Prompt = "Enter your city", Order = 3)]
+    [Required(ErrorMessage = "City is required")]
+    public string City { get; set; } = null!;
+}
---
+++ WebApp/ViewModels/AccountDetailsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using WebApp.Models;
+
+namespace WebApp.ViewModels;
+
+public class AccountDetailsViewModel
+{
+    public string Title { get; set; } = "Account Details";
+
+    public AccountDetailsBasicInfoModel BasicInfo { get; set; } = new AccountDetailsBasicInfoModel()
+    {
+        ProfileImage = "images/images/image-account-details.svg",
+        FirstName = "John",
+        LastName = "Doe",
+        Email = "john.doe@domain.com"
+    };
+
+    public AccountDetailsAddressModel AddressInfo { get; set; } = new AccountDetailsAddressModel();
+}
