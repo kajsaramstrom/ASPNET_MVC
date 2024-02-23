0b7ce2428e8fb258944514273c12f53554c2bb43
Kajsa <kajsaramstrom@live.se>
2024-02-23 14:04:44 +0100

    newsletter validation completed

--- WebApp/Models/SignUpModel.cs
+++ WebApp/Models/SignUpModel.cs
@@ -16,25 +16,25 @@ public class SignUpModel
     [MinLength(2, ErrorMessage = "Enter your lastname")]
     public string LastName { get; set; } = null!;
 
-    [Display(Name = "Email address", Prompt = "Enter your email address", Order = 3)]
+    [Display(Name = "Email address", Prompt = "Enter your email address", Order = 2)]
     [DataType(DataType.EmailAddress)]
     [Required(ErrorMessage = "Email is required.")]
     [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Your email address is invalid.")]
     public string Email { get; set; } = null!;
 
-    [Display(Name = "Password", Prompt = "********", Order = 4)]
+    [Display(Name = "Password", Prompt = "********", Order = 3)]
     [DataType(DataType.Password)]
     [Required(ErrorMessage = "Password is required.")]
     [RegularExpression(@"^(?=.*[A-Z])(?=.*[\W_]).{8,}$", ErrorMessage = "Invalid password, must be a strong password.")]
     public string Password { get; set; } = null!;
 
-    [Display(Name = "Confirm Password", Prompt = "********", Order = 5)]
+    [Display(Name = "Confirm Password", Prompt = "********", Order = 4)]
     [DataType(DataType.Password)]
     [Required(ErrorMessage = "Password must be confirmed.")]
     [Compare(nameof(Password), ErrorMessage = "The passwords do not match.")]
     public string ConfirmPassword { get; set; } = null!;
 
-    [Display(Name = "I agree to the Terms & Conditions.", Order = 6)]
+    [Display(Name = "I agree to the Terms & Conditions.", Order = 5)]
     [CheckboxRequired(ErrorMessage = "You must accept the terms and conditions to proceed.")]
     public bool TermsAndConditions { get; set; } = false;
 }
