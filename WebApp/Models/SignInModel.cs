79711659c4573fc19766f6605a0a5b10fe018e66
Kajsa <kajsaramstrom@live.se>
2024-02-23 15:16:52 +0100

    sign in validation done

--- WebApp/Models/SignInModel.cs
+++ WebApp/Models/SignInModel.cs
@@ -7,11 +7,13 @@ public class SignInModel
     [Display(Name = "Email address", Prompt = "Enter your email address", Order = 0)]
     [DataType(DataType.EmailAddress)]
     [Required(ErrorMessage = "Email is required.")]
+    [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Your email address is invalid.")]
     public string Email { get; set; } = null!;
 
     [Display(Name = "Password", Prompt = "********", Order = 1)]
     [DataType(DataType.Password)]
     [Required(ErrorMessage = "Password is required.")]
+    [RegularExpression(@"^(?=.*[A-Z])(?=.*[\W_]).{8,}$", ErrorMessage = "Invalid password, must be a strong password.")]
     public string Password { get; set; } = null!;
 
     [Display(Name = "Remember me", Order = 2)]
