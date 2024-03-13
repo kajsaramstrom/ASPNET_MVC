7d463bfed0e20bcc2d665fb0c90048661a751fab
Kajsa <kajsaramstrom@live.se>
2024-03-14 00:37:34 +0100

    mobile menu done

--- WebApp/wwwroot/js/profileavatar.js
+++ WebApp/wwwroot/js/profileavatar.js
@@ -2,3 +2,8 @@
     var dropdownMenu = document.getElementById("profileDropdownMenu");
     dropdownMenu.style.display = dropdownMenu.style.display === "block" ? "none" : "block";
 }
+
+function toggleDropdownMobile() {
+    var dropdownMenuMobile = document.getElementById("profileDropdownMenuMobile");
+    dropdownMenuMobile.style.display = dropdownMenuMobile.style.display === "block" ? "none" : "block";
+}
