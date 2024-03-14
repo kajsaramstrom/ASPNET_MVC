55740a81cbe9ec0ea1c5f1f6170acc04c1a1db39
Kajsa <kajsaramstrom@live.se>
2024-03-14 14:05:10 +0100

    darkmode homepage

---
+++ WebApp/wwwroot/js/lightAndDarkMode.js
@@ -0,0 +1,36 @@
+﻿var isDarkMode = false;
+var logoElement = document.getElementById("logo");
+var logoImage = "/images/silicon-logo.svg";
+logoElement.innerHTML = "<img src='" + logoImage + "' alt='Silicon Logo'>";
+
+function toggleBothSwitches() {
+    isDarkMode = !isDarkMode;
+    var desktopSwitch = document.getElementById("switch");
+    desktopSwitch.checked = isDarkMode;
+
+
+    var mobileSwitch = document.getElementById("switch-mobile");
+    mobileSwitch.checked = isDarkMode;
+    if (isDarkMode) {
+        activateDarkMode();
+    } else {
+        deactivateDarkMode();
+    }
+
+    updateLogo();
+}
+
+
+function activateDarkMode() {
+    document.body.classList.add("dark-mode");
+}
+
+function deactivateDarkMode() {
+    document.body.classList.remove("dark-mode");
+}
+
+
+function updateLogo() {
+    logoImage = isDarkMode ? "/images/images/logo-header-darkmode.svg" : "/images/logotypes/solid.svg";
+    logoElement.innerHTML = "<img src='" + logoImage + "' alt='Silicon Logo'>";
+}
