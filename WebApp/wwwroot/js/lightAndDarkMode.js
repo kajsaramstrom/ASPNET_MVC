74f882b40738fc5ee9553b71726db70706cf2e0c
Kajsa <kajsaramstrom@live.se>
2024-03-26 13:30:17 +0100

    light dark mode

--- WebApp/wwwroot/js/lightAndDarkMode.js
+++ WebApp/wwwroot/js/lightAndDarkMode.js
@@ -1,15 +1,79 @@
-﻿var isDarkMode = localStorage.getItem("darkMode") === "true" ? true : false;
+﻿//var isDarkMode = localStorage.getItem("darkMode") === "true" ? true : false;
+//var logoElement = document.getElementById("logo");
+//var logoImage = "/images/logotypes/solid.svg";
+//logoElement.innerHTML = "<img src='" + logoImage + "' alt='Silicon Logo'>";
+//var notFoundElement = document.getElementById("errorImage");
+//var notFoundImage = isDarkMode ? "/images/images/404darkmode.svg" : "/images/images/error404.svg";
+//var workWithUsElement = document.getElementById("workwithus");
+//var workWithUsImage = "/images/images/workwithus-image.svg";
+//workWithUsImage = isDarkMode ? "/images/images/workwithus-image-darkmode.svg" : "/images/images/workwithus-image.svg";
+//workWithUsElement.innerHTML = "<img src='" + workWithUsImage + "' alt='work with us image'>";
+//var desktopSwitch = document.getElementById("switch");
+//var mobileSwitch = document.getElementById("switch-mobile");
+//desktopSwitch.checked = isDarkMode;
+//mobileSwitch.checked = isDarkMode;
+
+
+//function toggleBothSwitches() {
+//    isDarkMode = !isDarkMode;
+//    localStorage.setItem("darkMode", isDarkMode);
+
+
+//    if (isDarkMode) {
+//        activateDarkMode();
+//    } else {
+//        deactivateDarkMode();
+//    }
+
+//    updateLogo();
+//    update404();
+//    workWithUs();
+//}
+
+
+//function activateDarkMode() {
+//    document.body.classList.add("dark-mode");
+//}
+
+//function deactivateDarkMode() {
+//    document.body.classList.remove("dark-mode");
+//}
+
+
+//function updateLogo() {
+//    logoImage = isDarkMode ? "/images/images/logo-header-darkmode.svg" : "/images/logotypes/solid.svg";
+//    logoElement.innerHTML = "<img src='" + logoImage + "' alt='Silicon Logo'>";
+//}
+
+//function update404() {
+//    notFoundImage = isDarkMode ? "/images/images/404darkmode.svg" : "/images/images/error404.svg";
+//    notFoundElement.innerHTML = "<img src='" + notFoundImage + "' alt='Page not found image'>";
+//}
+
+//function workWithUs() {
+//    workWithUsImage = isDarkMode ? "/images/images/workwithus-image-darkmode.svg" : "/images/images/workwithus-image.svg";
+//    workWithUsElement.innerHTML = "<img src='" + workWithUsImage + "' alt = 'work with us image'>"
+//}
+//window.onload = function () {
+//    if (isDarkMode) {
+//        activateDarkMode();
+//    }
+//    else {
+//        deactivateDarkMode();
+//    }
+//    updateLogo();
+//    update404();
+//    workWithUs();
+//}
+
+var isDarkMode = localStorage.getItem("darkMode") === "true" ? true : false;
 var logoElement = document.getElementById("logo");
-var logoImage = "/images/logotypes/solid.svg";
-logoElement.innerHTML = "<img src='" + logoImage + "' alt='Silicon Logo'>";
+var logoImage = isDarkMode ? "/images/images/logo-header-darkmode.svg" : "/images/logotypes/solid.svg";
 var notFoundElement = document.getElementById("errorImage");
 var notFoundImage = isDarkMode ? "/images/images/404darkmode.svg" : "/images/images/error404.svg";
-var workWithUsElement = document.getElementById("workwithus");
-var workWithUsImage = "/images/images/workwithus-image.svg";
-workWithUsImage = isDarkMode ? "/images/images/workwithus-image-darkmode.svg" : "/images/images/workwithus-image.svg";
-workWithUsElement.innerHTML = "<img src='" + workWithUsImage + "' alt='work with us image'>";
 var desktopSwitch = document.getElementById("switch");
 var mobileSwitch = document.getElementById("switch-mobile");
+logoElement.innerHTML = "<img src='" + logoImage + "' alt='Silicon Logo'>";
 desktopSwitch.checked = isDarkMode;
 mobileSwitch.checked = isDarkMode;
 
@@ -18,6 +82,8 @@ function toggleBothSwitches() {
     isDarkMode = !isDarkMode;
     localStorage.setItem("darkMode", isDarkMode);
 
+    desktopSwitch.checked = isDarkMode;
+    mobileSwitch.checked = isDarkMode;
 
     if (isDarkMode) {
         activateDarkMode();
@@ -27,13 +93,13 @@ function toggleBothSwitches() {
 
     updateLogo();
     update404();
-    workWithUs();
 }
 
 
 function activateDarkMode() {
     document.body.classList.add("dark-mode");
 }
+
 
 function deactivateDarkMode() {
     document.body.classList.remove("dark-mode");
@@ -46,22 +112,18 @@ function updateLogo() {
 }
 
 function update404() {
+
     notFoundImage = isDarkMode ? "/images/images/404darkmode.svg" : "/images/images/error404.svg";
-    notFoundElement.innerHTML = "<img src='" + notFoundImage + "' alt='Page not found image'>";
+    notFoundElement.innerHTML = "<img src='" + notFoundImage + "' alt='404 Not Found'>";
 }
 
-function workWithUs() {
-    workWithUsImage = isDarkMode ? "/images/images/workwithus-image-darkmode.svg" : "/images/images/workwithus-image.svg";
-    workWithUsElement.innerHTML = "<img src='" + workWithUsImage + "' alt = 'work with us image'>"
-}
+// Körs när sidan laddas för att sätta temaläget och uppdatera logotypen
 window.onload = function () {
     if (isDarkMode) {
         activateDarkMode();
-    }
-    else {
+    } else {
         deactivateDarkMode();
     }
     updateLogo();
     update404();
-    workWithUs();
-}
+};
